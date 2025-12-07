d15cafdea27ffc86d1df08e4644284fbed16a4d1
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-12-07 14:58:55 -0300

    .

---
+++ Hello_word.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	printf("\nHello Word!");
+	
+	return 0;
+}
---
+++ ex01_tipo_variavel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	int numero = 6;
+	float numero_quebrado = 50.49;
+	char letra = 'A';
+	char nome[7] = "Rafael";
+	
+	printf("Numero: %d", numero);
+	printf("\nFlutuante: %.2f", numero_quebrado);
+	printf("\nLetra: %c", letra);
+	printf("\nNome: %s", nome);
+	
+	return 0;
+}
