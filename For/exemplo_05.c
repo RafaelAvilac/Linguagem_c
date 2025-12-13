dc6a06ef4c2fd577adefd82929a67ec6d98c02a8
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-12-13 09:58:10 -0300

    .

--- For/exemplo_05.c
+++ For/exemplo_05.c
@@ -3,9 +3,9 @@
 int main(){
 	
 	for(int i = 1; i <= 10; i++){
-		printf("===============\n");
+		printf("=============\n");
 		printf("Tabuada do %d\n", i);
-		printf("===============\n");
+	
 			for(int j = 0; j <=10; j++){
 				printf("%d x %d = %d\n", i, j, i * j);
 				
