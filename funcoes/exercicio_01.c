eb664f449427fc30f4134cdcd23cf43f952b76fa
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-12-17 21:06:36 -0300

    .

---
+++ funcoes/exercicio_01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+float verificar_media(float nota_1, float nota_2){
+	
+	float media;
+	
+	media = (nota_1 + nota_2) / 2;
+	
+	return media;
+	
+}
+
+int main (){
+	
+	float nota_1, nota_2, media_final;
+	
+	printf("Informe a primeira nota: ");
+	scanf("%f", &nota_1);
+	
+	printf("Informe a segunda nota: ");
+	scanf("%f", &nota_2);
+	
+	media_final = verificar_media(nota_1, nota_2);
+	
+	printf("Media das duas notas foi %.2f", media_final);
+	
+	return 0;
+}
