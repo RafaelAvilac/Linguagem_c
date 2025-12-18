67cc67be01e14d7813d9524fc83236341263dadb
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-12-17 22:09:42 -0300

    .

---
+++ funcoes/exercicio_03.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/*3. Encontrar o maior de três números
+
+Crie uma função que receba três inteiros e retorne o maior deles.*/
+
+int verificar_maior(int a, int b, int c){
+	int maior = 0;
+	if (a > b && a > c){
+		
+		maior = a;
+	}else if(b > a && b > c){
+		
+		maior = b;
+	}else {
+		
+		maior = c;
+	}
+	return maior;
+}
+void preencher_variaveis(int *a,int *b,int *c){
+	
+	
+	printf("Informe o primeiro numero:");
+	scanf("%d", a);
+	printf("Informe o primeiro numero:");
+	scanf("%d", b);
+	printf("Informe o primeiro numero:");
+	scanf("%d", c);
+	
+}
+int main(){
+	int maior, a, b, c;
+	preencher_variaveis(&a, &b, &c);
+	maior = verificar_maior(a, b, c);
+	printf("O maior numero e %d", maior);
+	
+	return 0;
+}
---
+++ funcoes/exercicio_04.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*4. Classificar notas de alunos
+Crie uma função que receba a nota de um aluno e imprima:
+
+Aprovado (nota >= 6)
+
+Recuperação (nota >= 4 e < 6)
+
+Reprovado (nota < 4)*/
+
+void verificar_nota(float nota){
+	
+	if(nota >= 6){
+		printf("Aprovado\n");
+	}else if(nota >= 4 && nota < 6){
+		printf("Recuperacao\n");
+	}else{
+		printf("Reprovado.");
+	}	
+}
+
+int main (){
+	
+	float nota;
+	
+	printf("Informe sua nota: ");
+	scanf("%f", &nota);
+		while(nota < 0 || nota > 10){
+   			 printf("Nota invalida. Informe novamente: ");
+   			scanf("%f", &nota);
+		}
+	verificar_nota(nota);
+
+	return 0;
+}
