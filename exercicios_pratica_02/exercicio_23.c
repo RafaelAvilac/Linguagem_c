7a58c1869fca277cc7c61026566aeed9f5a3b542
RafaelAvilac <rafael_avilac@yahoo.com.br>
2026-01-11 16:47:47 -0300

    .

---
+++ exercicios_pratica_02/exercicio_23.c
@@ -0,0 +1,34 @@
+/*Exercício 16 — String básica (primeiro contato)
+Enunciado
+
+Declare uma string para armazenar um nome
+Leia o nome do usuário
+Mostre:
+o nome digitado
+o tamanho do nome (quantos caracteres)*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+
+	char nome[50];
+
+	
+	printf("Digite um nome: ");
+	fgets(nome, 50, stdin);
+	
+	printf("Nome: %s", nome);
+
+     size_t	tamanho = strlen(nome);
+     
+     if (tamanho > 0 && nome[tamanho - 1] == '\n'){
+     	
+     	nome[tamanho - 1] = '\0';
+     	tamanho--;
+	 }
+	
+	printf("Tamanho: %d", tamanho);
+	
+	return 0;
+}
