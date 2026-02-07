dbd77ebf65d2d2d8434fc61050cae3c7314d64dc
RafaelAvilac <rafael_avilac@yahoo.com.br>
2026-02-07 13:22:08 -0300

    .

--- exercicios_pratica/bloco_de _exercicios_04/cadastro_simples.c
+++ exercicios_pratica/bloco_de _exercicios_04/cadastro_simples.c
@@ -11,7 +11,7 @@ apenas maiores de idade
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
+#include <locale.h>
 typedef struct{
 	
 	char nome[100];
@@ -30,21 +30,32 @@ void preencher_dados(Cadastro *pessoas, int qtd){
 	printf("\n\nDados dos alunos.\n");
 	for(int i = 0; i < qtd; i++){
 		int entrada_valida = 0;
+		int nome_valido;
 		 do {
-            printf("Informe o %d nome: ", i + 1);
+		 	nome_valido = 1;
+            printf("Informe o %dº nome: ", i + 1);
             fgets(pessoas[i].nome, sizeof(pessoas[i].nome), stdin);
 			pessoas[i].nome[strcspn(pessoas[i].nome, "\n")] = '\0';
 			maiuscula(pessoas[i].nome);
             
-            if(strlen(pessoas[i].nome) == 0) {
+            if(!strlen(pessoas[i].nome)) {
                 printf("Erro: Nome não pode estar vazio!\n");
-            }
-        } while(strlen(pessoas[i].nome) == 0);
+                nome_valido = 0;
+            }else {
+				for(int j = 0; pessoas[i].nome[j] != '\0'; j++){
+					if(!isalpha( (unsigned char)pessoas[i].nome[j]) && !isspace((unsigned char)pessoas[i].nome[j])){
+						nome_valido = 0;
+						printf("Erro: O nome deve conter apenas letras e espacos.\n");
+						break;
+					}
+				}
+			}
+        } while(!nome_valido);
 		
 		while(!entrada_valida){
-			printf("Informe a idade do aluno: \n");
+			printf("Idade do aluno: ");
 			if(scanf("%d", &pessoas[i].idade) != 1){
-				printf("Digite apenas numeros");
+				printf("Digite apenas numeros\n");
 				while(getchar() != '\n');
 			}else if( pessoas[i].idade < 4){
 				printf("Idade mínima 4 anos.\n");
@@ -52,18 +63,27 @@ void preencher_dados(Cadastro *pessoas, int qtd){
 				entrada_valida = 1;
 				while(getchar() != '\n');
 			}
-			
 		}
-	
 	}
+	
+}
+
+void exibir_todos(Cadastro *pessoas, int qtd){
+	
+	printf("\n\n<<- Todos os alunos ->>\n");
+		for(int i = 0; i < qtd; i++){
+		
+			printf("%d: %-40s | %d anos\n", i+1, pessoas[i].nome, pessoas[i].idade);
+			printf("-----------------------\n");
+		}
 }
 int main(){
-	
+	setlocale(LC_ALL, "Portuguese_Brazil");
 	Cadastro *pessoas;
 	int qtd, entrada_valida = 0;
 	
 while(!entrada_valida){
-        printf("\nQunatos alunos deseja cadastrar: ");
+        printf("\nQuantos alunos deseja cadastrar: ");
         if(scanf("%d", &qtd) != 1) {
             printf("Erro: Digite apenas números!\n");
              while(getchar() != '\n');
@@ -82,7 +102,7 @@ while(!entrada_valida){
   		return 1;
 	  }
 	preencher_dados(pessoas, qtd);
-	
+	exibir_todos(pessoas, qtd);
 	free(pessoas);
 	return 0;
 }
