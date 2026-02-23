2b1a8c7d5855bf5dfd921fd0cd98992df52aaeb0
RafaelAvilac <rafael_avilac@yahoo.com.br>
2026-02-22 21:18:48 -0300

    .

---
+++ exercicios_pratica/variaveis_expressoes/14.c
@@ -0,0 +1,11 @@
+/* 14. Leia um angulo em graus e apresente-o convertido em radianos. A formula de conversao
+e: R = G * p /180, sendo G o angulo em graus e ˆ R em radianos e p = 3:14.*/
+
+#include <stdio.h>
+
+int main(void){
+	
+	printf("");
+	
+	return 0;
+}
