66635891dc94a04c77a6cc52eb6f156b1a049f1e
RafaelAvilac <rafael_avilac@yahoo.com.br>
2026-02-28 02:23:38 -0300

    .

---
+++ exercicios_pratica/variaveis_expressoes/32.c
@@ -0,0 +1,21 @@
+/*32. Leia um numero inteiro e imprima a soma do sucessor de seu triplo com o antecessor de ´
+seu dobro.*/
+
+#include <stdio.h>
+
+int main(void){
+	
+	int n, sucessor_triplo, antecessor_dobro, resultado;
+	
+	printf("Inforeme um numero inteiro: ");
+	scanf("%d", &n);
+	
+	 sucessor_triplo = n * 3 + 1;
+	 antecessor_dobro = n * 2 - 1;
+	 resultado =  sucessor_triplo + antecessor_dobro;
+	 
+	 printf("Resultado: %d", resultado);
+	
+	
+	return 0;
+}
